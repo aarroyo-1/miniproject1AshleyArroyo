f801771fd10d9cdf129e4e455657d04c4650d174
aarroyo-1 <a_arroyo@mail.fhsu.edu>
2025-02-16 20:29:12 -0600

    Setup yfinance to pull 5 of my favorite tickers. Made a simple dictionary.

--- main.py
+++ main.py
@@ -13,3 +13,20 @@ integrity statement may result in consequences, including disciplinary actions a
  and outlined in institutional policies. By signing this statement, I acknowledge my commitment to upholding the
  principles of academic integrity.
 '''
+
+#Imports
+import pprint
+import yfinance as yf
+
+#tickers Amazon, Spotify, Ebay, Google, Microsoft
+mytickers = ["AMZN", "SPOT", "EBAY", "GOOG", "MSFT"]
+
+mydata = {}
+
+mytickers.sort()
+for ticker in mytickers:
+    result = yf.Ticker(ticker)
+    mydata[ticker] = {'ticker': ticker,
+                      'dailyHigh': result.info['dayHigh']
+                        }
+pprint.pprint(mydata)
